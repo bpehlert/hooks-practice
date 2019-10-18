01dbca6cd1b6f450cb753c21f0191433da879365
Benjamin Ehlert <benjamin.ehlert@gmail.com>
2019-10-18 16:52:41 -0600

    added loading ternary check

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world.</h1>
-      <div>{content}</div>
+      {fetchedData.length === 0 ? "Loading..." : <div>{content}</div>}
     </div>
   );
 }
